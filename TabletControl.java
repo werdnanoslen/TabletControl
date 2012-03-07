460f04416f8b6b0015766f090ac4eb67e1d7634c
Andrew Nelson <werdnanoslen@gmail.com>
2012-03-07 14:13:01 -0500

    fixed preexisting errors

--- TabletControl.java
+++ TabletControl.java
@@ -3,71 +3,79 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class TabletControl extends Frame implements ActionListener
+public class TabletControl
 {	
-	final String CALNORMAL = "xrandr -o normal && xinput set-int-prop \"Fujitsu Component USB Touch Panel\" \"Evdev Axis Calibration\" 86 3873 280 3993";
-	final String CALCW = "xrandr -o left && xinput set-int-prop \    "Fujitsu Component USB Touch Panel\" \"Evdev Axis Calibration\" 86 3873 280     3993";
-	final String CALCCW = "xrandr -o right && xinput set-int-prop \    "Fujitsu Component USB Touch Panel\" \"Evdev Axis Calibration\" 86 3873 280     3993";
-	final String KEYBOARD = "caribou";
-	
-	Button normal, cw, ccw, keyboard;
-	
-	public static void main(String[] args)
-	{	
-		TabletControl tabletControl = new TabletControl("TabletControl");
-		tabletControl.setVisible(true);
-		
-		tabletControl.setAlwaysOnTop(true);
-		tabletControl.pack();
-		tabletControl.setLocation(0,0);
-	
-		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-			public void run(){
-				try {
-					Runtime.getRuntime().exec("xinput set-int-prop \"Fujitsu Component USB Touch Panel\" \"Evdev Axis Calibration\" 32 74 3883 280 397 8");
-				} catch (IOException e){
-					e.printStackTrace();
-				}
-			}
-		}));
-	}
-	
-	public TabletControl(String title)
-	{
-		super(title);
+    static final String CALNORM = "xrandr -o normal";/* && xinput set-int-prop"
+	+ " \"Fujitsu Component USB Touch Panel\""
+	+ " \"Evdev Axis Calibration\" 86 3873 280 3993";*/
+    static final String CALCW = "xrandr -o left";/* && xinput set-int-prop"
+	+ " \"Fujitsu Component USB Touch Panel\""
+	+ " \"Evdev Axis Calibration\" 86 3873 280 3993";*/
+    static final String CALCCW = "xrandr -o right";/* && xinput set-int-prop"
+	+ " \"Fujitsu Component USB Touch Panel\""
+	+ " \"Evdev Axis Calibration\" 86 3873 280 3993";*/
+    static final String KEYBOARD = "florence";
 
-		normal = new Button("normal");
-		cw = new Button("cw");
-		ccw = new Button("ccw");
-		keyboard = new Button("keyboard");
-	
-		add(normal);
-		add(cw);
-		add(ccw);
-		add(keyboard);		
-		
-		normal.addActionListener(this);
-		cw.addActionListener(this);
-		ccw.addActionListener(this);
-		keyboard.addActionListener(this);
-	}
+    Button normal, cw, ccw, keyboard;
 
-	public void actionPerformed(ActionEvent e)
-	{
-		try {		
-			if (e.getSource() == normal){
-				Runtime.getRuntime().exec(CALNORMAL);
-			} else if (e.getSource() == cw){
-				Runtime.getRuntime().exec(CALCW);
-			} else if (e.getSource() == ccw){
-				Runtime.getRuntime().exec(CALCCW);
-			} else if (e.getSource() == keyboard){
-				Runtime.getRuntime().exec(KEYBOARD);
-			} else {
-				;
-			}
-		} catch (IOException ee){
-			ee.printStackTrace();
-		}
-	}
+    public static void main(String[] args)
+    {	
+	TabletControl tabletControl = new TabletControl("TabletControl");
+	
+    }
+
+    public TabletControl(String title)
+    {
+	JFrame frame = new JFrame(title);
+	
+	frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+	JButton normal = new JButton("normal");
+	JButton cw = new JButton("cw");
+	JButton ccw = new JButton("ccw");
+	JButton keyboard = new JButton("keyboard");
+
+	frame.add(normal, BorderLayout.CENTER);
+	frame.add(cw, BorderLayout.LINE_END);
+	frame.add(ccw, BorderLayout.LINE_START);
+	frame.add(keyboard, BorderLayout.PAGE_END);		
+
+	normal.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent e)
+		{
+			try {Runtime.getRuntime().exec(CALNORM);}
+			catch (IOException ioe){ ioe.printStackTrace();}
+		}});
+	cw.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent e)
+		{
+			try {Runtime.getRuntime().exec(CALCW);}
+			catch (IOException ioe){ioe.printStackTrace();}
+		}});
+	ccw.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent e)
+		{
+			try {Runtime.getRuntime().exec(CALCCW);}
+			catch (IOException ioe){ ioe.printStackTrace();}
+		}});
+	keyboard.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent e)
+		{
+			try {Runtime.getRuntime().exec(KEYBOARD);}
+			catch (IOException ioe){ ioe.printStackTrace();}
+		}});
+	
+	frame.addWindowListener(new WindowAdapter(){
+		public void windowClosing(WindowEvent e)
+    		{
+			try {Runtime.getRuntime().exec(CALNORM);}
+			catch (IOException ioe){ ioe.printStackTrace();}
+			System.exit(0);
+		}});
+	frame.setAlwaysOnTop(true);
+	frame.pack();
+	frame.setLocation(0,0);
+	frame.setVisible(true);
+    }
+
 }
